35149b9971eca8174035ee0ffe68d79b0bd25f92
= <=>
2024-02-22 20:00:19 +0300

    update after adding home.html and making changes to the landing page

--- HabitTracker/urls.py
+++ HabitTracker/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from habit_tracker.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+
+
 ]
+
