b046f3efeadc576d747016a571f7debdd8112f9c
= <=>
2024-04-03 21:26:42 +0300

    Update after creating a model for the signup form

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Signup(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    occupation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
